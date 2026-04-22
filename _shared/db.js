1335753654ccd1e30367c1279a75881c49505e11
Robson Oliveira <robsonoliveira.ifrn@gmail.com>
2026-04-22 19:19:37 -0300

    fix: corrigido carregamento infinito no dashboard (faltando exportações no db.js)

--- _shared/db.js
+++ _shared/db.js
@@ -1,8 +1,10 @@
 import { db } from "../firebase-config.js";
 import {
   doc, getDoc, setDoc, updateDoc,
-  collection, addDoc, onSnapshot, serverTimestamp
+  collection, addDoc, onSnapshot, serverTimestamp,
+  query, where, getDocs
 } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 export { db, doc, getDoc, setDoc, updateDoc,
-         collection, addDoc, onSnapshot, serverTimestamp };
+         collection, addDoc, onSnapshot, serverTimestamp,
+         query, where, getDocs };
